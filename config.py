b0f1e5fe608d13360fb0707be7424c63ee55bc4a
marc_schaefer <marc-.schaefer@stud.unibas.ch>
2023-03-28 02:06:58 +0200

    Add capability to load multiple spec files at once

--- config.py
+++ config.py
@@ -5,16 +5,21 @@ connection = "lan"
 #connection = "serial"
 #connection = "stdin"
 
-# only used for "lan"
-address = ("192.168.1.253", 7053)
+# only used for "lan" - ip or domain name
+address = ("thermal.lan", 7053)
 vbus_pass = "vbus"
 
 # only used for "serial"
 port = "/dev/ttyAMA0"
 baudrate = 9600
 
-# specify Resol specs file
-spec_file = os.path.dirname(__file__) + '/spec/DeltaSolBS2009.json'
+# directory containing specs files
+spec_dir = os.path.dirname(__file__) + '/spec/'
+# specify one or more Resol specs files to use (Python tuple / comma separated)
+spec_files = ['DeltaSolBS2009.json', 'DeltaSolBX.json']
+# spec_files = ['DeltaSolSLL.json']
+# Deprecated - specify Resol specs file (still working but disables loading of multiple specs)
+# spec_file = os.path.dirname(__file__) + '/spec/DeltaSolBS2009.json'
 
 # expected amount of different source packets (see spec_file)
 expected_packets = 1
--- spec.py
+++ spec.py
@@ -5,23 +5,36 @@ import json
 import sys
 import config
 
+spec = {"device": [], "packet": []}
+
+
 # Load given specFile. Specfile was created from original
 # RESOL Configuration File XML shippes with RSC (Resol Service Center)
 # using XML to JSON converter at http://www.utilities-online.info/xmltojson
-with open(config.spec_file) as json_data:
-    data = json.load(json_data)
-    try:
-        spec = data['vbusSpecification']
-    except:
-        sys.exit('Cannot load Spec')
+def load_spec_file(spec_file):
+    global spec
+    with open(spec_file) as json_data:
+        data = json.load(json_data)
+        try:
+            spec['device'].extend(data['vbusSpecification']['device'])
+            spec['packet'].extend(data['vbusSpecification']['packet'])
+        except Exception as e:
+            sys.exit('Cannot load Spec: ' + str(e))
 
-if config.debug:
-    for device in spec['device']:
-        print(device)
+    if config.debug:
+        for device in spec['device']:
+            print(device)
 
-    for packet in spec['packet']:
-        print(packet)
-        for field in packet['field']:
-            print("  " + str(field))
+        for packet in spec['packet']:
+            print(packet)
+            for field in packet['field']:
+                print("  " + str(field))
 
-json_data.close()
+    json_data.close()
+
+
+if config.spec_files and len(config.spec_files) > 0:
+    for spec_file in config.spec_files:
+        load_spec_file(config.spec_dir + spec_file)
+else:
+    load_spec_file(config.spec_file)
